20b5a7eceb4446529563f68f9cda196ece7439d0
AnnaPeretiatka <annaperetiatka@gmail.com>
2025-08-12 12:31:52 +0000

    hh

--- jenkins_ci_demo_v2/demo-repo/utils/string_utils.py
+++ jenkins_ci_demo_v2/demo-repo/utils/string_utils.py
@@ -2,4 +2,4 @@
 def normalize_name(name: str) -> str:
     if not isinstance(name, str):
         raise TypeError('name must be a string')
-    return ' '.join(part.capitalize() for part in name.strip().split())
+    retur ' '.join(part.capitalize() for part in name.strip().split())
